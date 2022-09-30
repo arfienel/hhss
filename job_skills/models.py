604409aa13ea40a082717117d1d3017721d871f7
arfienel <intrershaman@gmail.com>
2022-09-30 10:43:34 +0400

    test

--- job_skills/models.py
+++ job_skills/models.py
@@ -12,7 +12,6 @@ class JobTracker(models.Model):
     subscribers = models.ManyToManyField(User, related_name='job_tracker_subs')
 
     def __str__(self):
-        bobobobo = 'клим мама папа максим тихан '
         return f'<Tracker {self.search_text} {self.id}>'
 
     def __repr__(self):
